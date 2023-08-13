802b420a8bcf573c274cef11539ed622df72373e
H-Q7 <hussam.a.qannam@gmail.com>
2023-08-13 20:15:07 +0300

    .

--- dist/scripts/scroll.js
+++ dist/scripts/scroll.js
@@ -10,42 +10,65 @@ function scrollHandler() {
         ((screenHeight - scrollSection.getClientRects()[0].top) /
             (screenHeight * 2)) *
         100;
-    console.log(scrollPercentage);
-    scrollElements[3].style = `transform: translateY(${
+    document.body.style = `background-color: hsl(0, 0%, ${
+        750 > scrollPercentage && scrollPercentage > 25
+            ? scrollPercentage - 17
+            : scrollPercentage > 100
+            ? 58
+            : 8
+    }%);`;
+    scrollElements[3].style = `transform: translate(${
+        150 > scrollPercentage && scrollPercentage > 120
+            ? (scrollPercentage - 120) * (10 / 3)
+            : scrollPercentage > 150
+            ? 100
+            : 0
+    }%,-${
         50 > scrollPercentage && scrollPercentage > 0
             ? 100 - scrollPercentage * 2
             : 50 < scrollPercentage
             ? 0
             : 100
     }%);`;
-    scrollElements[2].style = `transform: translateY(-${
+    scrollElements[2].style = `transform: translate(${
+        150 > scrollPercentage && scrollPercentage > 120
+            ? (scrollPercentage - 120) * (20 / 3)
+            : scrollPercentage > 150
+            ? 200
+            : 0
+    }%,${
         100 > scrollPercentage && scrollPercentage > 50
             ? 100 - (scrollPercentage - 50) * 2
             : 100 < scrollPercentage
             ? 0
             : 100
     }%);`;
-    scrollElements[1].style = `transform: translateY(${
-        150 > scrollPercentage && scrollPercentage > 100
-            ? 100 - (scrollPercentage - 100) * 2
-            : 150 < scrollPercentage
+    scrollElements[1].style = `transform: translate(-${
+        150 > scrollPercentage && scrollPercentage > 120
+            ? (scrollPercentage - 120) * (20 / 3)
+            : scrollPercentage > 150
+            ? 200
+            : 0
+    }%,-${
+        100 > scrollPercentage && scrollPercentage > 50
+            ? 100 - (scrollPercentage - 50) * 2
+            : 100 < scrollPercentage
             ? 0
             : 100
     }%);`;
-    scrollElements[0].style = `transform: translateY(-${
-        200 > scrollPercentage && scrollPercentage > 150
-            ? 100 - (scrollPercentage - 150) * 2
-            : 200 < scrollPercentage
+    scrollElements[0].style = `transform: translate(-${
+        150 > scrollPercentage && scrollPercentage > 120
+            ? (scrollPercentage - 120) * (10 / 3)
+            : scrollPercentage > 150
+            ? 100
+            : 0
+    }%,${
+        50 > scrollPercentage && scrollPercentage > 0
+            ? 100 - scrollPercentage * 2
+            : 50 < scrollPercentage
             ? 0
             : 100
     }%);`;
-    if (scrollPercentage > 210) {
-        document.querySelector(".fixed-section").classList.add("opacity-0");
-        document.querySelector(".after-scroll-section").classList.remove("opacity-0");
-    } else {
-        document.querySelector(".fixed-section").classList.remove("opacity-0");
-        document.querySelector(".after-scroll-section").classList.add("opacity-0");
-    }
 }
 
 document.addEventListener("scroll", event => {
